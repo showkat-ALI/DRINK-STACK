4113e0a8cc50b71fad221f27612358240369fa89
Showkat  Ali <uniqueshowkat114724@gmail.comi>
2022-01-09 00:54:45 +0600

    well-its-neve

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/my-app/build")));
   app.get("*", (req, res) => {
     res.sendFile(
-      path.join(__dirname, "frontend", "my-app", "build", "index.js")
+      path.join(__dirname, "frontend", "my-app", "build", "index.html")
     );
   });
 } else {
